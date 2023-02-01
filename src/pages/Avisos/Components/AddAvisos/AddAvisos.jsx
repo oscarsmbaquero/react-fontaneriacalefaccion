70a557c76c070f1db3dbb04ea0f54f80b847d18b
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-02-01 08:34:48 +0100

    addAviso with filter client

--- src/pages/Avisos/Components/AddAvisos/AddAvisos.jsx
+++ src/pages/Avisos/Components/AddAvisos/AddAvisos.jsx
@@ -7,15 +7,21 @@ import axios from "axios";
 import Swal from "sweetalert2"; // hay que probarlo
 import { Button } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import SearchInput from '../../../../core/SearchInput/SearchInput'
+import SearchInput from "../../../../core/SearchInput/SearchInput";
 
 const AddAvisos = () => {
   const [clientes, setClientes] = useState([]);
-  const [keyword, setKeyword] = useState('');
-  const [selectedJob, setSelectedJob] = useState('');
+  //const [keyword, setKeyword] = useState("");
+  const [relatedData, setRelatedData] = useState({});
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  console.log(selectedOption,18);
+
+
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isValid },
   } = useForm({ mode: "onChange" });
   let navigate = useNavigate();
@@ -26,26 +32,35 @@ const AddAvisos = () => {
       setClientes(res.data);
     };
     fetchClientes();
-  },[]); 
-    //Capturamos eel valor del input del buscador  y lo seteamos a keyword pasandolo a minusculas
-    const onInputChange = (e) => {
-      setKeyword(e.target.value.toLowerCase());
-    };
-    /*Con el valor introducido en el inpute del buscador filtramos los trabajos almacenaos en jobs,
-    Filtramos por empresa o por puestos ofertados, previo paso a minusculas*/
-    const filteredClientes = clientes.filter((client) =>
-      client.cliente.toLowerCase().includes(keyword)||
-      client.dni.toLowerCase().includes(keyword)
-    );
+  }, []);
 
-  console.log(clientes);
+  useEffect(() => {
+    const fetchClientesId = async () => {
+      const res = await axios.get(`${BASE_URL}/clientes/${selectedOption}`);
+      setRelatedData(res.data);
+    };
+    fetchClientesId();
+  }, [selectedOption]);
+
+
+  console.log(relatedData,46)
+  //Capturamos eel valor del input del buscador  y lo seteamos a keyword pasandolo a minusculas
+  // const onInputChange = (e) => {
+  //   setKeyword(e.target.value.toLowerCase());
+  // };
+  /*Con el valor introducido en el inpute del buscador filtramos los trabajos almacenaos en jobs,
+    Filtramos por empresa o por puestos ofertados, previo paso a minusculas*/
+  // const filteredClientes = clientes.filter((client) =>
+  //   client.cliente.toLowerCase().includes(keyword)||
+  //   client.dni.toLowerCase().includes(keyword)
+  // );
+
+  //console.log(filteredClientes);
   const onSubmit = async (formData) => {
-   
-    
     try {
-      const cobrado ='No Cobrado';
-      formData = {...formData,cobrado}
-      console.log(formData,'formData')
+      const cobrado = "No Cobrado";
+      formData = { ...formData, cobrado };
+      console.log(formData, "formData");
       const result = await fetch(`${BASE_URL}/avisos`, {
         method: "POST",
         headers: {
@@ -60,20 +75,20 @@ const AddAvisos = () => {
         },
         body: JSON.stringify(formData),
       });
-      if(result.status === 200 && resultado.status === 200){
+      if (result.status === 200 && resultado.status === 200) {
         Swal.fire({
           //title: "Success!",
           text: "Aviso introducido y cliente creado",
           icon: "success",
           confirmButtonText: "Ok",
         });
-      } else if(result.status === 200 && resultado.status === 401){
+      } else if (result.status === 200 && resultado.status === 401) {
         Swal.fire({
           //title: "Success!",
           text: "Aviso introducido, cliente ya en el sistema",
           icon: "success",
           confirmButtonText: "Ok",
-        });        
+        });
       }
       navigate("/");
     } catch (error) {
@@ -82,16 +97,39 @@ const AddAvisos = () => {
   };
   return (
     <>
-    <div className="form">
-      <div className="container">
-        <section className="sectionForm row">
-          <div className="col-12 col-lg-11 mx-3">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <br />
-              <div className="d-flex flex-column flex-md-row">
-                <div className="d-flex flex-column col-11 col-md-6 my-2">
-                  {/* <label className="form__label">Cliente * </label> */}
-                  <input
+      {/* <section className='searchContainer'>
+        <SearchInput placeholder="Buscar Cliente" onChange={onInputChange} />
+      </section> */}
+
+      <div className="form">
+        <div className="container">
+          <section className="sectionForm row">
+            <div className="col-12 col-lg-11 mx-3">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <br />
+                <div className="d-flex flex-column flex-md-row">
+                  <div className="d-flex flex-column col-11 col-md-6 my-2">
+                  <select
+                    name="jobs"
+                    className="form-control"
+                    value={selectedOption}
+                      onChange={(e) => setSelectedOption(e.target.value)}
+                  >
+                    <option
+                      // selected
+                      // value="638e32a42062209de55fd347"
+                      // class="bold-option"
+                    >
+                      {/* No hay consumo */}
+                    </option>
+                    {clientes.map((el) => (
+                      <option key={el._id} value={el._id} >
+                        {el.cliente}
+                      </option>
+                    ))}
+                  </select>
+                    {/* <label className="form__label">Cliente * </label> */}
+                    {/* <input
                     className="form-control"
                     type="text"
                     name="cliente"
@@ -99,116 +137,129 @@ const AddAvisos = () => {
                     {...register("cliente",{
                       required: "Campo Obligatorio",
                     })}
-                  />
-                  {/* <SearchInput placeholder="Filtrar por trabajo o empresa" onChange={onInputChange} /> */}
-                   {errors.cliente && errors.cliente.type === "required" && (
-                <p className="error">{errors.cliente.message}</p>
-              )}
+                  /> */}
+                    {/* <select
+                      value={selectedOption}
+                      onChange={(e) => setSelectedOption(e.target.value)}
+                    >
+                      <option value="option1">Opción 1</option>
+                      <option value="option2">Opción 2</option>
+                      <option value="option3">Opción 3</option>
+                    </select> */}
+
+                    {errors.cliente && errors.cliente.type === "required" && (
+                      <p className="error">{errors.cliente.message}</p>
+                    )}
+                  </div>
+                  <div className="d-flex flex-column col-11 col-md-6 mx-md-3 my-2">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="direccion"
+                      placeholder="Dirección"
+                      value={relatedData.direccion}
+                      // {...setValue("direccion",filteredClientes.direccion)}
+                      {...register("direccion", {
+                        required: "Campo Obligatotio",
+                      })}
+                    />
+                    {errors.direccion &&
+                      errors.direccion.type === "required" && (
+                        <p className="error">{errors.direccion.message}</p>
+                      )}
+                  </div>
                 </div>
-                <div className="d-flex flex-column col-11 col-md-6 mx-md-3 my-2">
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="direccion"
-                    placeholder="Dirección"
-                    {...register("direccion", {
-                  required: "Campo Obligatotio",
-                })}
-              />
-              {errors.direccion && errors.direccion.type === "required" && (
-                <p className="error">{errors.direccion.message}</p>
-              )}
+                <div className="d-flex flex-column flex-md-row">
+                  <div className="d-flex flex-column col-11 col-md-4 my-2">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="localidad"
+                      placeholder="Localidad"
+                      value={relatedData.localidad}
+                      {...register("localidad", {
+                        required: "Campo Obligatotio",
+                      })}
+                    />
+                    {errors.localidad &&
+                      errors.localidad.type === "required" && (
+                        <p className="error">{errors.localidad.message}</p>
+                      )}
+                  </div>
+                  <div className="d-flex flex-column col-11 col-md-4 mx-md-3 my-2">
+                    <input
+                      className="form-control"
+                      type="tel"
+                      name="telefono"
+                      placeholder="Teléfono"
+                      value={relatedData.telefono}
+                      {...register("telefono", {
+                        required: "Campo Obligatotio",
+                      })}
+                    />
+                  </div>
+                  <div className="d-flex flex-column col-11 col-md-4 my-2">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="caldera"
+                      placeholder="Tipo de Caldera"
+                      {...register("caldera", {
+                        required: "Campo Obligatotio",
+                      })}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="d-flex flex-column flex-md-row">
-                <div className="d-flex flex-column col-11 col-md-4 my-2">
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="localidad"
-                    placeholder="Localidad"
-                    {...register("localidad", {
-                  required: "Campo Obligatotio",
-                })}
-              />
-              {errors.localidad && errors.localidad.type === "required" && (
-                <p className="error">{errors.localidad.message}</p>
-              )}
+                <div className="d-flex flex-column flex-md-row">
+                  <div className="d-flex flex-column col-11 col-lg-12 col-md-6 mx-md-1 my-2">
+                    <textarea
+                      className="form-control"
+                      type="text"
+                      name="averia"
+                      placeholder="Descripción averia"
+                      {...register("averia", {
+                        required: "Campo Obligatotio",
+                      })}
+                    />
+                    {errors.averia && errors.averia.type === "required" && (
+                      <p className="error">{errors.averia.message}</p>
+                    )}
+                  </div>
                 </div>
-                <div className="d-flex flex-column col-11 col-md-4 mx-md-3 my-2">
-                  <input
-                    className="form-control"
-                    type="tel"
-                    name="telefono"
-                    placeholder="Teléfono"
-                    {...register("telefono", {
-                  required: "Campo Obligatotio",
-                })}
-              />
+                <div className="d-flex flex-column flex-md-row">
+                  <div className="d-flex flex-column col-11 col-md-6 my-2">
+                    <select {...register("prioridad")} className="form-control">
+                      <option value="Urgente">Urgente</option>
+                      <option value="Normal">Normal</option>
+                    </select>
+                  </div>
+                  <div className="d-flex flex-column col-11 col-md-6 mx-md-3 my-2">
+                    <select {...register("estado")} className="form-control">
+                      <option value="Abierto" selected>
+                        Abierto
+                      </option>
+                    </select>
+                  </div>
                 </div>
-                <div className="d-flex flex-column col-11 col-md-4 my-2">
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="caldera"
-                    placeholder="Tipo de Caldera"
-                    {...register("caldera", {
-                  required: "Campo Obligatotio",
-                })}
-              />
-                </div>
-              </div>
-              <div className="d-flex flex-column flex-md-row">
-                <div className="d-flex flex-column col-11 col-lg-12 col-md-6 mx-md-1 my-2">
-                  <textarea
-                    className="form-control"
-                    type="text"
-                    name="averia"
-                    placeholder="Descripción averia"
-                    {...register("averia", {
-                  required: "Campo Obligatotio",
-                })}
-              />
-              {errors.averia && errors.averia.type === "required" && (
-                <p className="error">{errors.averia.message}</p>
-              )}
-                  
-                </div>
-              </div>
-              <div className="d-flex flex-column flex-md-row">
-                <div className="d-flex flex-column col-11 col-md-6 my-2">
-                  <select {...register("prioridad")} className="form-control">
-                    <option value="Urgente">Urgente</option>
-                    <option value="Normal">Normal</option>
-                  </select>
-                </div>
-                <div className="d-flex flex-column col-11 col-md-6 mx-md-3 my-2">
-                  <select {...register("estado")} className="form-control" >
-                    <option value="Abierto" selected>Abierto</option>
-                    
-                  </select>
-                  
-                </div>
-              </div>
-              <Button
-                variant="contained"
-                disabled={!isValid}
-                type="submit"
-                endIcon={<SendIcon />}
-                style={{
-                  borderRadius: 50,
-                  backgroundColor: "primary",
-                  color: "white",
-                  marginTop:'30px'
-                }}
-              >
-                Enviar
-              </Button>
-            </form>
-          </div>
-        </section>
+                <Button
+                  variant="contained"
+                  disabled={!isValid}
+                  type="submit"
+                  endIcon={<SendIcon />}
+                  style={{
+                    borderRadius: 50,
+                    backgroundColor: "primary",
+                    color: "white",
+                    marginTop: "30px",
+                  }}
+                >
+                  Enviar
+                </Button>
+              </form>
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
     </>
   );
 };
